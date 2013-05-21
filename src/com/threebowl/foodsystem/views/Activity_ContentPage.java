ff209ee8b3776929227b630c6dafac5f7ef02d6c
Administrator <Administrator@PCoS-20111212VZ>
2013-05-21 21:39:11 +0800

    内容页面加载图标完善【发布版】

--- src/com/threebowl/foodsystem/views/Activity_ContentPage.java
+++ src/com/threebowl/foodsystem/views/Activity_ContentPage.java
@@ -3,10 +3,12 @@ package com.threebowl.foodsystem.views;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.threebowl.foodsystem.MainActivity;
 import com.threebowl.foodsystem.R;
 import com.threebowl.foodsystem.base.HttpDate;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -33,6 +35,9 @@ public class Activity_ContentPage extends Activity {
 	private ImageButton mImageButton_Back;
 	private String url_a;
 	private HttpDate date;
+	private MyHandler myHandler;
+	private ProgressDialog pDialog;
+	private String  mString_html;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -54,29 +59,77 @@ public class Activity_ContentPage extends Activity {
 		});
 		
 		Intent intent = getIntent();
+		myHandler = new MyHandler();
 		url_a = new String();
 		url_a = "http://home.meishichina.com/" + intent.getStringExtra("FoodURL");// 将bundle中提取String即a标签的数据
 		date = new HttpDate();
-		String tempHtml = date.GetHtml(url_a);//获取到完整的html页面数据
-		String tempCut = date.String_Cut(tempHtml);//截取有用数据
-		tempCut = tempCut.replaceAll("&nbsp;", "");
-		List<String> tempUrls = new ArrayList<String>();
-		List<String> tempSpans = date.GetSpan(tempCut);
-		List<Bitmap> bitmaps = new ArrayList<Bitmap>();
 		
+		MyThread thread = new MyThread();
+		new Thread(thread).start();
+		
+		pDialog = getMypDialog();
+		pDialog.show();
+		
+	}
+	
+	/**
+	 * 等待图标对话框
+	 */
+	public ProgressDialog getMypDialog() {
+		ProgressDialog mpDialog = new ProgressDialog(Activity_ContentPage.this);
+		mpDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//		mpDialog.setTitle("请稍等");// 设置标题
+//		mpDialog.setIcon(R.drawable.icon_logo);// 设置图标
+		mpDialog.setMessage("正在加载您要的数据...");
+		mpDialog.setIndeterminate(false);// 设置进度条是否为不明确
+		mpDialog.setCancelable(false);// 设置进度条是否可以按退回键取消
+		return mpDialog;
+	}
+	
+	class MyThread implements Runnable{
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Message msg = new Message();
+			
+			mString_html = date.GetHtml(url_a);//获取到完整的html页面数据
+			
+			Activity_ContentPage.this.myHandler.sendMessage(msg);
+		}
+	}
+	
+	
+	class MyHandler extends Handler{
+		
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
 
-		try {
-			tempUrls = date.getData(date.String2InputStream(tempCut));
-			for (int i = 0; i < tempUrls.size(); i++) {
-				bitmaps.add(date.getBitmap(tempUrls.get(i)));
+			
+			String tempCut = date.String_Cut(mString_html);//截取有用数据
+			tempCut = tempCut.replaceAll("&nbsp;", "");
+			List<String> tempUrls = new ArrayList<String>();
+			List<String> tempSpans = date.GetSpan(tempCut);
+			List<Bitmap> bitmaps = new ArrayList<Bitmap>();
+			
+
+			try {
+				tempUrls = date.getData(date.String2InputStream(tempCut));
+				for (int i = 0; i < tempUrls.size(); i++) {
+					bitmaps.add(date.getBitmap(tempUrls.get(i)));
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
+			Adapter_Contnet adapter = new Adapter_Contnet(Activity_ContentPage.this, tempSpans,bitmaps);
+			mListView_Span.setAdapter(adapter);
+			
+			pDialog.dismiss();
 			
 		}
-		Adapter_Contnet adapter = new Adapter_Contnet(Activity_ContentPage.this, tempSpans,bitmaps);
-		mListView_Span.setAdapter(adapter);
-
 	}
 	
 	
